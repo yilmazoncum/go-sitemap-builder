c99a9c6b5233c6d8bf2d75fce2a35027cc2f09f2
yilmazoncum <oncumkorkmaz@gmail.com>
2022-10-24 15:28:43 +0300

    getting web page

--- main.go
+++ main.go
@@ -1,26 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"strings"
-
-	link "main/linkParser"
+	"io"
+	"net/http"
+	"os"
 )
 
-var exampleHtml = `
-<html>
-<body>
-  <h1>Hello!</h1>
-  <a href="/other-page">A link to another page</a>
-</body>
-</html>
-`
-
 func main() {
-	r := strings.NewReader(exampleHtml)
-	links, error := link.Parse(r)
-	if error != nil {
-		panic(error)
+	urlFlag := flag.String("url", "https://gophercises.com", "the url that you want to build a sitemap for")
+	flag.Parse()
+
+	fmt.Println(*urlFlag)
+
+	response, err := http.Get(*urlFlag)
+	if err != nil {
+		panic(err)
 	}
-	fmt.Printf("%+v\n", links)
+	defer response.Body.Close()
+	io.Copy(os.Stdout, response.Body)
 }
