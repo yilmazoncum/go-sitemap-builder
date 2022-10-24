4a0ab1dc3c781769c9ab513261bfc8d03f0182dc
yilmazoncum <oncumkorkmaz@gmail.com>
2022-10-24 13:02:49 +0300

    DFS'ing the HTML

---
+++ examples/ex1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"link"
+	"strings"
+)
+
+var exampleHtml = `
+<html>
+<body>
+  <h1>Hello!</h1>
+  <a href="/other-page">A link to another page</a>
+</body>
+</html>
+`
+
+func main() {
+	r := strings.NewReader(exampleHtml)
+	links, error := link.Parse(r)
+	if error != nil {
+		panic(error)
+	}
+	fmt.Printf("%+v\n", links)
+}
